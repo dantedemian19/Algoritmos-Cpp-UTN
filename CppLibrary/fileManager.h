a9d7d936c470bb5b03d6405816344142609c2466
dantedemian19 <35985947+dantedemian19@users.noreply.github.com>
2022-05-13 18:24:57 -0300

    update dynamic and file manager
    update to node class to public for easy use in other instances.
    , and changes to file manager logic, <not tested>.
    2nd progrma started.

--- CppLibrary/fileManager.h
+++ CppLibrary/fileManager.h
@@ -1,22 +1,59 @@
 #pragma once
 #include <fstream>
+#include "./dynamicTypes.h"
 
 using std::string;
 
 template <typename dataclass>
-class fileManager<dataclass> {
+class fileManager {
 private:
 	string file;
-	
 public:
-	dataclass read(string fileName);
+
+	linkClass<dataclass> inMemoryFile; 
+	void read(dataclass data);
+	void readToMemory();
 	void write(dataclass data);
 };
 
 template <typename dataclass>
-dataclass fileManager<dataclass>::read(string fileName) {
-	file = fileName + ".txt";
+void fileManager<dataclass>::readToMemory() {
 	fstream fileObj = fstream(file, ios::in);
+	dataclass temp;
+	if(!fileObj.fail()) while(!fileObj.eof()) {
+		fileObj >> temp;
+		inMemoryFile.addToEnd(temp);
+	}
+	else{
+		cout<< "error reading the file"
+		exit; 
+	}
+	fileObj.close();
+};
 
-	
+template <typename dataclass>
+template <typename datatype>
+void fileManager<dataclass>::read(datatype data){
+	fstream fileObj = fstream(file, ios::in);
+	if(!fileObj.fail()) fileObj >> data;
+	else{
+		cout<< "error reading the file"
+		exit; 
+	}
+	fileObj.close();
+};
+
+template <typename dataclass>
+void fileManager<dataclass>::write() {
+	linkClass<dataclass>::nodeClass* cursor = inMemoryFile->start;
+	fstream fileObj = fstream(file,ios::ate);
+	if(!fileObj.fail()) while(cursor!=nullptr) {
+		fileObj << cursor->data;
+		cursor = cursor->next;
+	}
+	else{
+		cout<< "error writing the file"
+		exit;
+	}
+	fileObj.close();
 };
--- Programs/2ndProgram.h
+++ Programs/2ndProgram.h
@@ -1,36 +1,68 @@
 #pragma once
 #include "../CppLibrary/Menu.h" 
 #include "../CppLibrary/dynamicTypes.h"
-#include <ctime>
 
-
-void main() {
-
-    const int menuOptions = 3;
-    string menuTitle = "\n\t programa 2: ejercicio de lista de fechas \n";
-    string menuText[menuOptions + 1] = {
-        "start",
-        " Ingresar Alumno",
-        " ver promedio y resultados",
-        "end"
+class program2{
+private:
+    class date{
+        public:
+            int day;
+            int month;
+            int year;
     };
-    menu.declare(menuOptions, 1, menuTitle);
-    while (menu.w != menu.exit) {
-        menu.menu(menuText);
-        switch (menu.w)
-        {
-        case 1:
-            inputData();
-            break;
-        case 2:
-            showData();
-            break;
-        case menuOptions:
 
-            break;
-        default:
-            errormens();
-            break;
+    class person{
+        date birthday;
+        date age;
+        string name;
+        void update(){
+            
+        };
+    };
+    class memory{
+        linkClass<person> people;
+        // date today =
+        person oldest;
+        person younger;
+        void update(){
+            linkClass<person>::nodeClass cursor = person->start;
+            while (cursor!=nullptr)
+            {
+                cursor.data.update();
+                if(){
+                    
+                }
+            }
+            
+        };
+    };
+public:   
+    void main() {
+        const int menuOptions = 3;
+        string menuTitle = "\n\t programa 2: ejercicio de lista de fechas \n";
+        string menuText[menuOptions + 1] = {
+            "start",
+            " Ingresar datos de persona",
+            " ver promedio y resultados",
+            "end"
+        };
+        menu.declare(menuOptions, 1, menuTitle);
+        while (menu.w != menu.exit) {
+            menu.menu(menuText);
+            switch (menu.w)
+            {
+            case 1:
+                inputData();
+                break;
+            case 2:
+                showData();
+                break;
+            case menuOptions:
+                break;
+            default:
+                errormens();
+                break;
+            }
         }
-    }
-};
+    };
+}
